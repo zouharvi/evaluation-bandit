7d7b9d922eb4f25a260b2ad76ef4595189c9063a
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-01-15 21:37:58 +0100

    update harmonic microplot

--- scripts/05-harmonic_microplot.py
+++ scripts/05-harmonic_microplot.py
@@ -3,17 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data generation
-x_line = np.linspace(1, 8, 500)
-y_line = 1 / x_line
-
-x_dots = np.arange(1, 8 + 1)
+x_dots = np.arange(1, 15)
 y_dots = 1 / x_dots
 
 # Plotting
 fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
-
-# Black line
-ax.plot(x_line, y_line, color="black", linewidth=1.5)
 
 # Little dots on natural numbers
 ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
@@ -23,25 +17,15 @@ ax.axis("off")
 
 # Save as SVG
 plt.tight_layout(pad=0)
-plt.savefig("../figures/harmonic_microplot.svg", transparent=True)
+plt.savefig("../figures/harmonic_microplot_pow1.svg", transparent=True)
 plt.show()
 
-# %%
-import matplotlib.pyplot as plt
-import numpy as np
-
 # Data generation
-x_line = np.linspace(1, 8, 500)
-y_line = 1 / x_line**0.3
-
-x_dots = np.arange(1, 8 + 1)
-y_dots = 1 / x_dots**0.3
+x_dots = np.arange(1, 15)
+y_dots = 1 / x_dots**2
 
 # Plotting
 fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
-
-# Black line
-ax.plot(x_line, y_line, color="black", linewidth=1.5)
 
 # Little dots on natural numbers
 ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
@@ -52,4 +36,23 @@ ax.axis("off")
 # Save as SVG
 plt.tight_layout(pad=0)
 plt.savefig("../figures/harmonic_microplot_pow3.svg", transparent=True)
+plt.show()
+
+
+# Data generation
+x_dots = np.arange(1, 15)
+y_dots = [1 if i <= 3 else 0 for i in x_dots]
+
+# Plotting
+fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
+
+# Little dots on natural numbers
+ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
+
+# No decorations, Tufte style (remove spines and ticks)
+ax.axis("off")
+
+# Save as SVG
+plt.tight_layout(pad=0)
+plt.savefig("../figures/harmonic_microplot_top.svg", transparent=True)
 plt.show()
