1342bcc716b3603749908d2e6661864d232f81c2
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-02-22 14:59:18 +0100

    constrained intro figure

--- scripts/12-intro_figure.py
+++ scripts/12-intro_figure.py
@@ -159,3 +159,96 @@ plot_trace(ax2, model_scores_bandit, "bandit")
 plt.tight_layout(pad=0.1)
 plt.savefig("../figures/intro_figure.svg")
 plt.show()
+
+# %%
+import evaluation_bandit.utils_fig
+import matplotlib.pyplot as plt
+
+progress = {
+    "model A": [1, 7, 10, 11, 13],
+    "model B": [
+        2,
+        5,
+        9,
+        12,
+    ],
+    "model C": [3, 6],
+    "model D": [4, 8],
+}
+
+
+def item_to_color(score):
+    return plt.cm.RdYlGn(score / 100)
+
+
+# have a grid 4x10 with some items (circles) filled left to right
+plt.figure(figsize=(4, 2.5))
+
+for model_i, (model, model_progress) in enumerate(progress.items()):
+    for i, x in enumerate(model_progress):
+        score = model_scores_all[model][i + 2]
+        color = item_to_color(score)
+        plt.plot(i, model_i, "o", markersize=18, color=color)
+        plt.text(i, model_i, x, fontsize=10, ha="center", va="center")
+    plt.scatter(
+        i + 1,
+        model_i,
+        s=340,
+        facecolors="lightgray",
+        edgecolors="black",
+        linewidths=1.5,
+        linestyles=":",
+    )
+
+
+for model_i, i, offset_x, offset_y in [
+    (0, 5, 0, 0.2),
+    (1, 4, 0, 0.2),
+    (2, 2, 0.3, 0),
+    (3, 2, 0.3, 0),
+]:
+    plt.annotate(
+        "",
+        xy=(i + offset_x, model_i + offset_y),
+        xytext=(4.4, 2.8),
+        arrowprops=dict(
+            arrowstyle="->", color="black", lw=1, connectionstyle="arc3,rad=-0.1"
+        ),
+    )
+
+# TODO: plot to the four last circles and describe it as "available model+item to evaluate next"
+plt.annotate(
+    "model A was\nevaluated last\n(step 13) on\nitem 5 with\nhigh score (green)",
+    xy=(4, 0.2),  # slightly top right from the circle at (4, 0)
+    xytext=(6, 2),
+    arrowprops=dict(
+        arrowstyle="->", color="black", lw=1, connectionstyle="arc3,rad=-0.1"
+    ),
+    fontsize=8,
+    ha="left",
+    va="bottom",
+)
+
+plt.text(
+    4.5,
+    3,
+    "available model+item\nfor $\\pi$ to evaluate next",
+    fontsize=8,
+    ha="left",
+    va="center",
+)
+
+
+# TODO: point to last model A's item (13) and say "the last evaluated item (13th) is model A on item 5 with high score (green)"
+
+
+plt.xlim(-0.5, 9.2)
+plt.ylim(-0.3, len(progress) - 0.7)
+plt.xticks(range(10), range(1, 11))
+plt.yticks(range(len(progress)), progress.keys())
+plt.gca().invert_yaxis()
+plt.xlabel("Evaluation item")
+plt.gca().spines[["top", "right"]].set_visible(False)
+plt.tight_layout(pad=0.1)
+plt.savefig("../figures/intro_figure_constrained.svg")
+plt.show()
