8511f46174b845b7d00d8680c00aa6d4cd3fb998
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-04-07 11:03:16 +0200

    microplot update

--- scripts/11-microplot.py
+++ scripts/11-microplot.py
@@ -1,83 +1,42 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-# Data generation
-x_dots = np.arange(1, 15)
-y_dots = 1 / x_dots
-
-# Plotting
-fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
-
-# Little dots on natural numbers
-ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
-
-# No decorations, Tufte style (remove spines and ticks)
-ax.axis("off")
-
-# Save as SVG
-plt.tight_layout(pad=0)
-plt.ylim(-0.1, 1.1)
-plt.savefig("../figures/microplot_weight_pow1.svg", transparent=True)
-plt.show()
-
-# Data generation
-x_dots = np.arange(1, 15)
-y_dots = 1 / x_dots**2
-
-# Plotting
-fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
-
-# Little dots on natural numbers
-ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
-
-# No decorations, Tufte style (remove spines and ticks)
-ax.axis("off")
-
-# Save as SVG
-plt.tight_layout(pad=0)
-plt.ylim(-0.1, 1.1)
-plt.savefig("../figures/microplot_weight_pow3.svg", transparent=True)
-plt.show()
+os.makedirs("../figures/microplot_weight", exist_ok=True)
 
 
-# Data generation
-x_dots = np.arange(1, 15)
-y_dots = [1 if i <= 3 else 0 for i in x_dots]
+def plot_microplot(name, fn):
+    # Data generation
+    x_dots = np.arange(1, 15)
+    y_dots = fn(x_dots)
 
-# Plotting
-fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
+    # Plotting
+    fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
 
-# Little dots on natural numbers
-ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
+    # Little dots on natural numbers
+    ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
 
-# No decorations, Tufte style (remove spines and ticks)
-ax.axis("off")
+    # No decorations, Tufte style (remove spines and ticks)
+    ax.axis("off")
 
-# Save as SVG
-plt.tight_layout(pad=0)
-plt.ylim(-0.1, 1.1)
-plt.savefig("../figures/microplot_weight_top3.svg", transparent=True)
-plt.show()
+    # Save as SVG
+    plt.tight_layout(pad=0)
+    plt.ylim(-0.1, 1.1)
+    plt.savefig(f"../figures/microplot_weight/{name}.svg", transparent=True)
+    plt.show()
 
-# Data generation
-x_dots = np.arange(1, 15)
-y_dots = [1 if i == 1 else 0 for i in x_dots]
 
-# Plotting
-fig, ax = plt.subplots(figsize=(1, 0.3))  # Small size for microplot
+plot_microplot("pow1", lambda x: 1 / x)
+plot_microplot("pow2", lambda x: 1 / x**2)
+plot_microplot("pow3", lambda x: 1 / x**3)
+plot_microplot("top3", lambda x: [1 if i <= 3 else 0 for i in x])
+plot_microplot("top1", lambda x: [1 if i == 1 else 0 for i in x])
+plot_microplot("bot3", lambda x: [1 if i >= len(x) - 2 else 0 for i in x])
+plot_microplot("middle3", lambda x: [1 if 6 <= i <= 8 else 0 for i in x])
+plot_microplot("pow0.5", lambda x: 1 / x**0.5)
+plot_microplot("revpow1", lambda x: 1 / (len(x) - x + 1))
 
-# Little dots on natural numbers
-ax.scatter(x_dots, y_dots, color="black", s=20, zorder=3, marker=".")
-
-# No decorations, Tufte style (remove spines and ticks)
-ax.axis("off")
-
-# Save as SVG
-plt.tight_layout(pad=0)
-plt.ylim(-0.1, 1.1)
-plt.savefig("../figures/microplot_weight_top1.svg", transparent=True)
-plt.show()
 
 # %%
 
